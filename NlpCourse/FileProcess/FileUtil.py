ff83f6461cab6228c90ebb740df3db53198c04d4
Jackie <zhubo812@gmail.com>
2019-01-29 10:35:57 +0800

    更新fileUtil

--- NlpCourse/FileProcess/FileUtil.py
+++ NlpCourse/FileProcess/FileUtil.py
@@ -19,9 +19,9 @@ class FileUtil(object):
         file.close()
         return content
     
-    def readlines(self,path,encode):
+    def readlines(self,path,encoding):
         lines = []
-        file = open(path, 'r',encoding=encode)
+        file = open(path, 'r',encoding=encoding)
         for line in file:
             lines.append(line)
         file.close()
@@ -30,11 +30,11 @@ class FileUtil(object):
     def getFiles(self,dir):
         pathList =[]
         path = os.path.abspath('..')+"/"+dir+"/"
-        print(path)
+#         print(path)
         for parent,dirnames,filenames in os.walk(path):    #三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
 #             for dirname in  dirnames:                       #输出文件夹信息
 #                 print(  "dirname is" + dirname)
-            print(filenames)
+#             print(filenames)
             for filename in filenames:
                 pathList.append(path+filename)
         return pathList
