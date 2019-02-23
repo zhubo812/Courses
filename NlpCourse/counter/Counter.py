abd8e2685a9f4adf5282ad9194556b815adcebb1
Jackie <zhubo812@gmail.com>
2019-02-23 10:26:08 +0800

    添加词频统计方法

---
+++ NlpCourse/counter/Counter.py
@@ -0,0 +1,24 @@
+'''
+Created on Feb 23, 2019
+
+@author: Jackie
+'''
+
+class Counter(object):
+    '''
+    classdocs
+    '''
+    dict
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        dic={}
+    def add(self,k):
+        dict[k]=dict[k]+1
+        
+    def addWithFreq(self,k,v):
+        dict[k]=dict[k]+v
+    def getResult(self):
+        return dict
