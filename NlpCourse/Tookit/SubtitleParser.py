b326cd7f8f29f9c57cb1ecd0c0e91a75fd5c8241
Jackie <zhubo812@gmail.com>
2021-12-24 14:29:30 +0800

    update

---
+++ NlpCourse/Tookit/SubtitleParser.py
@@ -0,0 +1,84 @@
+
+#!/usr/bin/python3
+ 
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://10.13.0.5:27017/")
+mydb = myclient["Media"]
+mycol = mydb["Subtitle"]
+
+
+
+
+def ASSParser():
+	paths = ['Better.Days.chs.ass'];
+	for path in paths:
+		file = open(path, 'r',encoding='utf-8')
+		# for line in file.readlines():
+		# 	print(line.strip())
+		lines = file.readlines()
+
+		# print(parts)
+		for line in lines:
+			if(len(line) == 0 or line.startswith("Dialogue:")==False):
+				continue
+			items=line.split(',')#分割字符串
+			# print(len(items))
+
+			startTime = items[1].strip()
+			endTime = items[2].strip()
+			text = items[9].strip().replace("{\\blur3}","")
+
+			print(startTime)
+			print(endTime)
+			print(text)
+			print()
+
+			subtDic = {}
+			subtDic['Type'] = 'Movie'
+			subtDic['Name'] ='少年的你'##根据不同的字幕文件修改名称
+			subtDic['Number'] = 1##根据不同的字幕文件修改序号
+			subtDic['startTime'] = startTime
+			subtDic['endTime'] = endTime
+			subtDic['text'] = text
+			
+			x = mycol.insert_one(subtDic)
+
+def SRTParser():
+	paths = ['4_Chinese.srt'];
+	for path in paths:
+		file = open(path, 'r',encoding='utf-8')
+		# for line in file.readlines():
+		# 	print(line.strip())
+		content = file.read()
+		parts = content.split('\n\n')
+		# print(parts)
+		for part in parts:
+			if(len(part) == 0):
+				continue
+			items=part.split('\n')
+			# print(len(items))
+			timestr = items[1]
+			timeArray = timestr.split('-->')
+			startTime = timeArray[0].strip()
+			endTime = timeArray[1].strip()
+			text = items[2]
+
+			subtDic = {}
+			subtDic['Type'] = 'Movie'
+			subtDic['Name'] ='中国爱情故事'##根据不同的字幕文件修改名称
+			subtDic['Number'] = 1##根据不同的字幕文件修改序号
+			subtDic['startTime'] = startTime
+			subtDic['endTime'] = endTime
+			subtDic['text'] = text
+			
+			x = mycol.insert_one(subtDic)
+
+
+
+# SRTParser()
+ASSParser()
+
+
+
+
--- NlpCourse/testUnit/nlpTest.py
+++ NlpCourse/testUnit/nlpTest.py
@@ -4,12 +4,18 @@ Created on 2019年1月28日
 
 @author: Jackie
 '''
-from NlpTookit.NlpTookit import NlpTookit
+# from NlpTookit.NlpTookit import NlpTookit
 
 
-line="自。然.语？言！处理?3.1415926。"
+# line="自。然.语？言！处理?3.1415926。"
 
-tookit = NlpTookit()
-sentences = tookit.toSentenceList(line)
-print(line)
-print(sentences)
+# tookit = NlpTookit()
+# sentences = tookit.toSentenceList(line)
+# print(line)
+# print(sentences)
+
+
+line = '中院'
+print(line[0])
+print(line[1:-1])
+print(line[-1])
