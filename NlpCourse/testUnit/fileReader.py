c1edf53b95d62be0d9c739a893accaa8fc1d969b
Jackie <zhubo812@gmail.com>
2021-12-30 12:20:34 +0800

    update

--- NlpCourse/testUnit/fileReader.py
+++ NlpCourse/testUnit/fileReader.py
@@ -22,25 +22,39 @@ def getFiles(direc):#首先要获取到指定文件夹(dir)内所有文本的路
 		filelist.append(filepath)
 	return filelist
 
+
+def getAllFiles(file_path):
+    os.chdir(file_path)
+    # print(os.path.abspath(os.curdir))
+    all_file = os.listdir()
+    files = []
+    for f in all_file:
+        if os.path.isdir(f):
+            files.extend(getAllFiles(file_path+'\\'+f))
+            os.chdir(file_path)
+        else:
+            files.append(file_path+'\\'+f)
+    return files
+
 wordlist=[]
 worddic = {}
-filelist = getFiles('docs')
+filelist = getAllFiles('E:/temp/2/sogoulex')
 print(filelist)
 
 # for filePath in filelist:
 # 	singlewordlist = open(filePath, 'r',encoding='utf-8').read().split()
 # 	for word in singlewordlist:
 # 		wordlist.append(word)
-for filePath in filelist:
-	file = open(filePath,'r',encoding='utf-8')
-	for line in file:
-		for word in line.split():
-			# wordlist.append(word)
-			if word in worddic:
-				worddic[word]= worddic[word]+1
-			else:
-				worddic[word]=1
+# for filePath in filelist:
+# 	file = open(filePath,'r',encoding='utf-8')
+# 	for line in file:
+# 		for word in line.split():
+# 			# wordlist.append(word)
+# 			if word in worddic:
+# 				worddic[word]= worddic[word]+1
+# 			else:
+# 				worddic[word]=1
 
-print(worddic)
+# print(worddic)
 
-print(len(worddic))
+# print(len(worddic))
